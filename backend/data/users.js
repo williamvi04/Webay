de3df4b7e78474604224b4960ed77ad88ff4d680
williamvi04 <william.jvillalta@gmail.com>
2022-09-26 16:14:24 -0600

    adding more users

--- backend/data/users.js
+++ backend/data/users.js
@@ -4,6 +4,7 @@ const users = [
     { name: 'Admin', email: 'admin@webay.com', password: bcryptjs.hashSync('12345678'), isAdmin: true },
     { name: 'William Villalta', email: 'william.jvillalta@gmail.com', password: bcryptjs.hashSync('123456')},
     { name: 'Juan Perez', email: 'juan.perez@gmail.com', password: bcryptjs.hashSync('123456')},
+    { name: 'user', email: 'user.new@gmail.com', password: bcryptjs.hashSync('123456')},
     
 ];
 
